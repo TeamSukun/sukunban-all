a9f99265c3614f5e527dfaaba963866f32ab13d8
TeamSukun <115199434+TeamSukun@users.noreply.github.com>
2023-03-19 16:51:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+class Config:
+    API_ID=1619182628
+    API_HASH="ksjs"
+    TOKEN="jaksks"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "1844390456").split(" "))
+    START_IMG="jdjsjw"
+    BOT_ID=1013
+    BOT_USERNAME="djsjw"
+    BOT_NAME="SUKUN BAN ALL"
